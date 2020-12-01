63e06ff7ec15084096831cec14c85b24dede6de3
annivajo <annivajo@gmail.com>
2020-12-01 16:30:47 +0200

    refactoring

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,15 @@
+import {Ingredient} from '../shopping-list/ingredient.model';
+
 export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imegePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = description;
-    this.imagePath = imegePath;
+    this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shopping-list/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -7,10 +8,20 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe('guacamole',
       'very delicious thingy',
-      'https://www.simplyrecipes.com/wp-content/uploads/2018/07/Guacamole-LEAD-1.jpg'),
+      'https://www.simplyrecipes.com/wp-content/uploads/2018/07/Guacamole-LEAD-1.jpg',
+      [
+        new Ingredient('Avokado', 3),
+        new Ingredient('Lime', 1),
+        new Ingredient('Tomatoes', 2)
+      ]),
     new Recipe('blinciki',
       'yammy',
-      'https://www.inspiredtaste.net/wp-content/uploads/2016/07/Pancake-Recipe-1-1200.jpg')
+      'https://www.inspiredtaste.net/wp-content/uploads/2016/07/Pancake-Recipe-1-1200.jpg',
+      [
+        new Ingredient('Flower', 1),
+        new Ingredient('Egg', 3),
+        new Ingredient('Vanilla', 2)
+      ])
   ];
   getRecepies() {
     return this.recipes.slice();
